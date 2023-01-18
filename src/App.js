5b4a2ca916199f42cc34492b095e9fff8fd15d96
Shyam <skjoshee007@gmail.com>
2023-01-18 19:24:37 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -2,43 +2,26 @@ import React from "react";
 import "./styles.css";
 const App = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="community">
-          <h2 style={{ color: "rgb(0,185,195)" }}> Join Our community</h2>
-          <br></br>
-          <h2 style={{ color: "rgb(100,158,36)" }}>
-            30-day,hassle free monkey back gurarantee
-          </h2>{" "}
-          <br></br>
-          <p className="1stpara" style={{ color: "rgb(149,166,197)" }}>
-            Gain access to our full library of tutorials along with expert code
-            reviews. Perfect for any developers who are serious about honing
-            their skills.
+    <div id="main">
+      <div class="container">
+        <div class="community">
+          <h2 class="community-header">Join our community</h2>
+          <p>30-day, hassle-free monkey back gurarantee</p>
+          <p>Gain access to our full library of tutorials along with experts code reviews. Perfect for any developers
+            who are serious about honing their skills.
           </p>
         </div>
-        <div className="popat">
-          <div className="card">
-            <h3 style={{ color: "antiquewhite" }}>Monthly Subscription</h3>
-            <h3 style={{ color: "antiquewhite" }}>
-              {" "}
-              <span>$29 </span> Per Month
-            </h3>
-            <h3 style={{ color: "antiquewhite" }}>
-              Full access for less tham $1 a day
-            </h3>
-            <br></br>
-
-            <button
-              className="btn"
-              style={{ color: "antiquewhite" }}
-              type="button"
-            >
-              <h3>Sign Up</h3>{" "}
-            </button>
+        <div class="card-details">
+          <div class="card">
+            <p class="card-header">Monthly Subscription</p>
+            <p>
+              <span>$29</span>
+              Per Month
+            </p>
+            <p>Full access for less than $1 a day.</p>
+            <button>Sign Up</button>
           </div>
-
-          <div className="why-us">
+          <div class="why-us">
             <h4 class="why-us-header">Why Us</h4>
             <ul className="why-us-items">
               <li>Tutorials by industry experts</li>
